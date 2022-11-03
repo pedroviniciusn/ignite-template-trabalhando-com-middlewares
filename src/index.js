6e4e66765e1d9b28d07516feefe9088d67e5edf9
Pedro Vinicius <pedrovinicius.dev@gmail.com>
2022-11-02 21:48:40 -0300

    adicionando middlewares checksExistsUserAccount e checksCreateTodosUserAvailability

--- src/index.js
+++ src/index.js
@@ -10,11 +10,33 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+  
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({
+      error: 'User not found'
+    })
+  }
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request
+
+  const quantityTodos = user.todos.length
+
+  const userPro = user.pro
+
+  if (!userPro && quantityTodos + 1 >= 10) {
+    return response.status(403).json()
+  }
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
